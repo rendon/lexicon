5b218e2cd6bb1b5c30de2ba474adb20343679c23
Rafael Rendon Pablo <rafaelrendonpablo@gmail.com>
2024-04-10 10:53:28 -0700

    Escape 'word' parameter

--- _utils/save-words-on-mw.sh
+++ _utils/save-words-on-mw.sh
@@ -3,17 +3,18 @@
 FILE=$1
 
 while read word; do
+  escaped_word=$(echo $word | sed 's/ /+/g')
   RES=$(curl 'https://www.merriam-webster.com/lapi/v1/wordlist/save' --compressed -X POST\
       -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0'\
       -H 'Accept: application/json, text/javascript, */*; q=0.01'\
       -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8'\
       -H "Cookie: $MERRIAM_WEBSTER_COOKIE"\
-      --data-raw 'word='$word'&type=d' 2>/dev/null)
+      --data-raw 'word='$escaped_word'&type=d' 2>/dev/null)
 
   if [[ $RES =~ "already exists" ]]
   then
-    echo "$word already saved"
+    echo "\"$word\" already saved"
   else
-    echo "Added $word"
+    echo "Added \"$word\""
   fi
 done <$FILE
